a0e7a39ab22d8aa9524e7088db9e97f08a0debe4
Kamila Adamska <121kama121@gmail.com>
2023-05-13 22:30:24 +0200

    Added gallery markup

--- src/index.js
+++ src/index.js
@@ -2,21 +2,59 @@ import axios from 'axios';
 
 const API_URL = 'https://pixabay.com/api';
 const API_KEY = '36365586-331bc85183b3fd7ba137836b3';
-const axios = require('axios');
+const galleryEl = document.querySelector('.gallery');
 
-async function getImages() {
+const getImages = async () => {
   try {
     const response = await axios.get(`${API_URL}`, {
       params: {
         key: API_KEY,
         q: 'yellow flowers',
         image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
       },
     });
-    console.log(response);
+    const images = response.data.hits;
+    return images;
   } catch (error) {
     console.error(error);
   }
-}
+};
 
-getImages();
+const makeGallery = async () => {
+  try {
+    const images = await getImages();
+    const markup = images
+      .map(image => {
+        return `<div class="photo-card">
+  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span>${image.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <span>${image.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span>${image.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span>${image.downloads}</span>
+    </p>
+  </div>
+</div>
+`;
+      })
+      .join('');
+    galleryEl.innerHTML = markup;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+makeGallery();
