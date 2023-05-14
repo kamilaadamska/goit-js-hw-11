0aa630495f70520446f80ea98755c1cede3e6d0e
Kamila Adamska <121kama121@gmail.com>
2023-05-14 03:49:46 +0200

    Finished all functions

--- src/index.js
+++ src/index.js
@@ -8,11 +8,19 @@ const API_KEY = '36365586-331bc85183b3fd7ba137836b3';
 const galleryEl = document.querySelector('.gallery');
 const formEl = document.getElementById('search-form');
 const inputEl = document.querySelector('input[class="form-element"]');
+const loadBtn = document.querySelector('.load-more');
+const PER_PAGE = 40;
+let pageNo;
+let totalPages;
+let markup;
+let searchedPhrase;
 
 formEl.addEventListener('submit', ev => {
   ev.preventDefault();
-  galleryEl.innerHTML = '';
-  makeGallery(inputEl.value.trim());
+  markup = '';
+  pageNo = 1;
+  searchedPhrase = inputEl.value.trim();
+  makeGallery(searchedPhrase);
 });
 
 const getImages = async searchedPhrase => {
@@ -24,24 +32,33 @@ const getImages = async searchedPhrase => {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
-        page: 1,
-        per_page: 40,
+        page: pageNo,
+        per_page: PER_PAGE,
       },
     });
     if (response.data.hits.length === 0) throw new Error();
-    return response.data.hits;
+    if (searchedPhrase === '') throw new Error();
+    if (pageNo === 1) {
+      Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+    }
+    totalPages = Math.ceil(response.data.totalHits / PER_PAGE);
+    return response.data;
   } catch (error) {
     console.error(error);
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
+    markup = '';
+    galleryEl.innerHTML = markup;
+    loadBtn.classList.add('is-hidden');
   }
 };
 
 const makeGallery = async searchedPhrase => {
   try {
-    const images = await getImages(searchedPhrase);
-    const markup = images
+    const data = await getImages(searchedPhrase);
+    const images = data.hits;
+    markup += images
       .map(image => {
         return `<div class="photo-card">
   <a href="${image.largeImageURL}"><img src="${image.webformatURL}" class="photo" alt="${image.tags}" loading="lazy" /></a>
@@ -68,8 +85,31 @@ const makeGallery = async searchedPhrase => {
       })
       .join('');
     galleryEl.innerHTML = markup;
-    new SimpleLightbox('.gallery a');
+    const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
+    loadBtn.classList.remove('is-hidden');
   } catch (error) {
+    console.error(error);
+  }
+};
+
+loadBtn.addEventListener('click', () => {
+  loadMore();
+});
+
+const loadMore = async () => {
+  try {
+    loadBtn.classList.add('is-hidden');
+    if (totalPages === pageNo) {
+      Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return;
+    }
+    pageNo++;
+    await makeGallery(searchedPhrase);
+    loadBtn.classList.remove('is-hidden');
+  } catch {
     console.error(error);
   }
 };
